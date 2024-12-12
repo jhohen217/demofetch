4f011826f4586032739f0e1392d81104b13220e4
Josh Hohenadel <josh@hohenadel.com>
2024-12-12 01:20:41 -0500

    fix: properly expose resscore functions in core package

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,10 @@ from .resscore import (
     get_category_counts
 )
 
+from .bot import DemoBot
+
 __all__ = [
+    'DemoBot',
     'calculate_storage_cost',
     'get_match_ids_count',
     'get_downloaded_match_ids_count',
